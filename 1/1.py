35dd3941c1dedff574e5c50e06431d2f43999ce7
Pavel Tsaturov <p.tsaturov@g.nsu.ru>
2026-02-05 15:37:50 +0700

    Add 1 task

---
+++ 1/1.py
@@ -0,0 +1,20 @@
+from icmplib import ping
+import csv
+
+domains = ["google.com", "yandex.ru", "youtube.com", "spotify.com", "dns.ru", 
+            "github.com", "vk.com", "yahoo.com", "mail.ru", "facebook.com"]
+
+results = [["domain", "ip", "packets_sent", "packets_received", 
+            "packets_lost", "rtt(ms)"]]
+
+for domain in domains:
+
+    response = ping(domain, count=1)
+    results.append([domain, response.address, response.packets_sent, 
+                    response.packets_received, response.packet_loss, response.avg_rtt])
+
+with open('results.csv','w', newline='') as file:
+
+    writer = csv.writer(file)
+    writer.writerow(results[0])
+    writer.writerows(results[1:])
